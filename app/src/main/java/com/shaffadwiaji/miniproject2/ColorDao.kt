7656387d003a46c4946bfe8933db84ffca67a336
ShaffaDwiaji <amagiri7141@google.com>
2025-03-19 21:21:37 +0700

    Update - membuat Dao dengan anotasi query, insert, update, dan delete

---
+++ app/src/main/java/com/shaffadwiaji/miniproject2/ColorDao.kt
@@ -0,0 +1,24 @@
+package com.shaffadwiaji.miniproject2
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ColorDao {
+
+    @Query("SELECT * FROM colors")
+    fun getAll(): List<Color>
+
+    @Insert
+    fun insert(vararg colors: Color)
+
+    @Update
+    fun update(color: Color)
+
+    @Delete
+    fun delete(color: Color)
+
+}
