0225686db0ef9afe3354231d56c68e78b69611fa
ShaffaDwiaji <amagiri7141@google.com>
2025-03-19 21:14:08 +0700

    Update - membuat Entity dengan Primarykey dan Columninfo

---
+++ app/src/main/java/com/shaffadwiaji/miniproject2/Color.kt
@@ -0,0 +1,12 @@
+package com.shaffadwiaji.miniproject2
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "colors")
+data class Color(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "hex_value") val hexValue: String?,
+    @ColumnInfo(name = "name") val name: String?
+)
